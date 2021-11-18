a5dfce4e619e541a1f1ded32a8bf8709f1516fde
joacozimer <joacozimerman@gmail.com>
2021-11-18 11:56:21 -0300

    holis

--- 4_cesar.c
+++ 4_cesar.c
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[]) {
   // el primer argumento es la clave para cifrar
   // la cantidad de letras a desplazar (un entero)
+  
   int key = atoi(argv[1]);
   // para cada arg restante tratado como un string cada uno
   // cifrarlos con la clave e imprimir el mensaje entero cifrado
--- libprueba.c
+++ libprueba.c
@@ -1,1 +1,31 @@
 // implementar las funciones de libprueba.h aca
+#include <stdio.h>
+#include <stdlib.h>
+
+void saludar(char *name){
+  printf("Hola, %s\n", name);
+}
+
+void print_array(int a[], int size){
+  for (int i = 0; i < size; i++) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+}
+
+int potencia_de_dos(int n){
+  int res = 1;
+  for (int i = 0; i < n; i++) {
+    res *= 2;
+  }
+  return res;
+  // printf("%d\n", n * 2);
+}
+
+
+void suma_vectorial(int u[], int v[], int result[], int n){
+  for (int i = 0; i < n; i++) {
+    result[i] = u[i] + v[i];
+  }
+  // printf("%d\n",result);
+}
